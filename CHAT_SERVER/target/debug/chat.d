6e5d473d6eb6959a35306e12139ef6434b91f582
serinkanasci <44199709+serinkanasci@users.noreply.github.com>
2021-10-18 11:05:49 +0200

    Delete CHAT_SERVER/target directory

--- CHAT_SERVER/target/debug/chat.d
+++
@@ -1,1 +0,0 @@
-/home/serinkan/Desktop/CHAT/target/debug/chat: /home/serinkan/Desktop/CHAT/src/main.rs
--- CHAT_SERVER/target/debug/deps/chat-d56bd44b4fb8e1af.d
+++
@@ -1,5 +0,0 @@
-C:\Users\PC\Desktop\CHAT\target\debug\deps\chat-d56bd44b4fb8e1af.exe: src\main.rs
-
-C:\Users\PC\Desktop\CHAT\target\debug\deps\chat-d56bd44b4fb8e1af.d: src\main.rs
-
-src\main.rs:
