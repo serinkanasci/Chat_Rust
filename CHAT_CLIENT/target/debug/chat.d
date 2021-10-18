f5d54d7a4f23c4c63f2db4b4dfd43e1a57ef8892
serinkanasci <44199709+serinkanasci@users.noreply.github.com>
2021-10-18 11:06:03 +0200

    Delete CHAT_CLIENT/target directory

--- CHAT_CLIENT/target/debug/chat.d
+++
@@ -1,1 +0,0 @@
-C:\Users\PC\Desktop\PROJET_RUST\CHAT_CLIENT\target\debug\chat.exe: C:\Users\PC\Desktop\PROJET_RUST\CHAT_CLIENT\src\main.rs
--- CHAT_CLIENT/target/debug/deps/chat-d56bd44b4fb8e1af.d
+++
@@ -1,5 +0,0 @@
-C:\Users\PC\Desktop\PROJET_RUST\CHAT_CLIENT\target\debug\deps\chat-d56bd44b4fb8e1af.exe: src\main.rs
-
-C:\Users\PC\Desktop\PROJET_RUST\CHAT_CLIENT\target\debug\deps\chat-d56bd44b4fb8e1af.d: src\main.rs
-
-src\main.rs:
